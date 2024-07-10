c9189227e49cd5eb394af6a22c21191d06d84e1c
이준서 <junseolee@ajou.ac.kr>
2024-07-11 01:05:37 +0900

    게임 오버 시 이미지 추가

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -12,6 +12,7 @@ public class UIManager : MonoBehaviour
     private GameObject restartButton;           // 시작 버튼 UI
     private Text restartText;                   // 시작 버튼에 들어가는 텍스트 UI
     private GameObject exitButton;              // 게임 종료 버튼 UI
+    private GameObject gameoverImage;           // 게임종료시 이미지
     
     public void InitUI(int level)
     {
@@ -20,11 +21,13 @@ public class UIManager : MonoBehaviour
         restartButton = GameObject.Find("RestartButton");
         restartText = GameObject.Find("RestartText").GetComponent<Text>();
         exitButton = GameObject.Find("ExitButton");
+        gameoverImage = GameObject.Find("GameoverImage");
 
         levelText.text = "Floor " + level;
         levelImage.SetActive(true);
         restartButton.SetActive(false);
         exitButton.SetActive(false);
+        gameoverImage.SetActive(false);
     }
 
     public void HideLevelImage()
@@ -34,8 +37,18 @@ public class UIManager : MonoBehaviour
 
     public void ShowGameOver(int level)
     {
+        gameoverImage.SetActive(true);                                          // 게임 오버 이미지 보이기
+
         restartText.text = "RESTART";
-        levelText.text = "After " + level + "floors, you failed.";   // 게임 오버 텍스트
+        levelText.text = "After " + level + "floors, you failed.";              // 게임 오버 텍스트
+        levelText.rectTransform.anchoredPosition = new Vector3(0f, -150f, 0f);   // 게임 오버 텍스트 위치 이동
+
+        RectTransform rectTransform;
+        rectTransform = restartButton.GetComponent<RectTransform>();            // 재시작 버튼 위치 이동
+        rectTransform.anchoredPosition = Vector3.zero;
+
+        rectTransform = exitButton.GetComponent<RectTransform>();               // 종료 버튼 위치 이동
+        rectTransform.anchoredPosition = new Vector3(0f, 60f, 0f);
 
         levelImage.SetActive(true);
         restartButton.SetActive(true);
