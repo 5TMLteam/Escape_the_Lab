d8f31efb4959a64ba484a5bba376ba0ed45f6d3f
과ㄱ <55085233+dittoes@users.noreply.github.com>
2024-07-11 22:26:12 +0900

    노션 참고 간단한 회복량 조정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,8 +10,8 @@ public class Player : MovingObject
 {
     /* public 변수 */
     public int wallDamage = 1;              // 1회 공격당 Wall에 가하는 데미지
-    public int pointsPerFood = 9;          // 음식을 먹었을 때 오르는 포만감
-    public int pointsPerSoda = 13;          // 소다를 먹었을 때 오르는 포만감
+    public int pointsPerFood = 10;          // 음식을 먹었을 때 오르는 포만감
+    public int pointsPerSoda = 15;          // 소다를 먹었을 때 오르는 포만감
     public int pointsPerPoison = -3;        // 독에 중독되었을 때 감소하는 포만감
 
     public float restartLevelDelay = 1f;    // 다음 스테이지로 넘어가는 지연 시간
